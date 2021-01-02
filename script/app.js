780054b93bf8070f7585fabbe0973fb6f060f85a
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-02 12:59:29 -0800

    commit added js to modal

--- script/app.js
+++ script/app.js
@@ -27,3 +27,24 @@ function showSlides(n) {
   dots[slideIndex - 1].className += "active";
 }
 
+// MODAL 
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('contactBtn');
+const span = document.getElementsByClassName('close')[0];
+
+// When user clicks on the button, open modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When user clicks anywhere outside of modal, close modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
